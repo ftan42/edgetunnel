e0ed9b00900bfa3dd27b695f1b665eae9c39b393
ftan42 <89956479+ftan42@users.noreply.github.com>
2025-05-18 21:27:04 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 let userID = '';
-let proxyIP = '';
+let ProxyIP = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
 let subConfig = atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FDTDRTU1IvQUNMNFNTUi9tYXN0ZXIvQ2xhc2gvY29uZmlnL0FDTDRTU1JfT25saW5lX01pbmlfTXVsdGlNb2RlLmluaQ==');
@@ -12,23 +12,23 @@ let socks5Address = '';
 let parsedSocks5Address = {};
 let enableSocks = false;
 
-let noTLS = 'false';
-const expire = 4102329600;//2099-12-31
+let Notls = 'false';
+const Expire = 4102329600;//2099-12-31
 let proxyIPs;
 let socks5s;
 let go2Socks5s = [
 	'*ttvnw.net',
 	'*tapecontent.net',
-	'*cloudatacdn.com',
+	'*clouddataacdn.com',
 	'*.loadshare.org',
 ];
 let addresses = [];
-let addressesapi = [];
-let addressesnotls = [];
-let addressesnotlsapi = [];
+let addressesapi = [https://raw.githubusercontent.com/ftan42/cfipcaiji/refs/heads/main/ip.txt];
+let addresses Notls = [];
+let addresses notlsapi = [];
 let addressescsv = [];
 let DLS = 8;
-let remarkIndex = 1;//CSV备注所在列偏移量
+let remarkIndex = 1;//CSV note column offset
 let FileName = atob('ZWRnZXR1bm5lbA==');
 let BotToken;
 let ChatID;
